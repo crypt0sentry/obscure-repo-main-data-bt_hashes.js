f2caa075c2b09d432b5959afc47ab260ab646120
crypt0sentry <goboosterbot@gmail.com>
2025-08-07 09:34:17 -0400

    Update bt_hashes.js

--- bt_hashes.js
+++ bt_hashes.js
@@ -1,8 +1,3 @@
-// bt_hashes.js
-// Passwords stored as SHA-256 hashes. Comments allowed.
-export const PASSWORD_HASHES = [
-  // Mike - main dev
-  "477b9a445784545cc7277c272547c486a08c195853fa84972e8af29542c95ce2",
-  // Tester1 - temp
-  "def456...anotherhash"
-];
+[
+  "477b9a45784545cc7277c272547c486a08c195853fa84972e8af29542c95ce2"
+]
